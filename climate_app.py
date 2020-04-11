d4e038b223f101e7a6d8779d2ccbf97bdb52ae95
Terry Potter <tepotter@yahoo.com>
2020-04-11 09:48:30 -0500

    Added last dynamic route to climate_app.py and tweaked jupyter notebook

--- climate_app.py
+++ climate_app.py
@@ -43,7 +43,7 @@ def welcome():
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/temp_stats_start/yyyy-mm-dd<br/>"
-        f"/api/v1.0/temp_stats_start_end/(yyyy-mm-dd)/(yyyy-mm-dd)"
+        f"/api/v1.0/temp_stats_start_end/yyyy-mm-dd/yyyy-mm-dd"
     )
 
 
@@ -107,30 +107,56 @@ def tobs():
     return jsonify(all_tobs)
 
 
-@app.route("/api/v1.0/temp_stats_start/<date>/")
-#@app.route("/api/v1.0/temp_stats_start/2010-08-23/")
-def temp_stats_start(date):
+@app.route("/api/v1.0/temp_stats_start/<start_date>/")
+def temp_stats_start(start_date):
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return the min temperature, the avg temperature, and the max temperature for a given start date"""
-    # Query temperature observations for all stations    
+    # Query temperature observations for all stations with given start date   
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),\
-     func.max(Measurement.tobs)).filter(Measurement.date >= date).all()
+     func.max(Measurement.tobs)).filter(Measurement.date >= start_date).all()
   
     session.close()
 
     # Create a dictionary from the row data and append to a list of all_tobs
     temp_stats = []
     for result in results:
-        row = {}
-        row['Start Date'] = date
-        row['Minimum Temperature'] = (result[0])
-        row['Average Temperature'] = (result[1])
-        row['Maximum Temperature'] = (result[2])
-        temp_stats.append(row)
+        dict_row = {}
+        dict_row['Start Date'] = start_date
+        dict_row['Minimum Temperature'] = (result[0])
+        dict_row['Average Temperature'] = (result[1])
+        dict_row['Maximum Temperature'] = (result[2])
+        temp_stats.append(dict_row)
 
     return jsonify(temp_stats)
+
+
+@app.route("/api/v1.0/temp_stats_start_end/<start_date>/<end_date>")
+def temp_stats_start_end(start_date, end_date):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return the min temperature, the avg temperature, and the max temperature for a given start date and end date"""
+    # Query temperature observations for all stations with given start and end dates   
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),\
+     func.max(Measurement.tobs)).filter(Measurement.date >= start_date, Measurement.date <= end_date).all()
+  
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_tobs
+    temp_stats_end = []
+    for result in results:
+        dict_row = {}
+        dict_row['Start Date'] = start_date
+        dict_row['End Date'] = end_date
+        dict_row['Minimum Temperature'] = (result[0])
+        dict_row['Average Temperature'] = (result[1])
+        dict_row['Maximum Temperature'] = (result[2])
+        temp_stats_end.append(dict_row)
+
+    return jsonify(temp_stats_end)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
